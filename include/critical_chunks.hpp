041d2357a2d995d1e5e035580e4d361a8d823f3a
Mikolaj <mikolajpaz@gmail.com>
2025-03-14 16:12:31 +0100

    Cleanup

--- include/critical_chunks.hpp
+++ include/critical_chunks.hpp
@@ -10,27 +10,6 @@
 #define byte_t unsigned char
 #endif
 
-// class IHDR {
-//     public:
-//         IHDR(std::ifstream &img)
-//             { read(img); }
-
-//         friend std::ostream& operator<<(std::ostream& out, const IHDR& obj);
-//         friend class PNGfile;
-            
-//     private:
-//         unsigned int width;
-//         unsigned int height;
-        
-//         byte_t bit_depth;
-//         byte_t color_type;
-//         byte_t compression_method;
-//         byte_t filter_method;
-//         byte_t interlace_method;
-
-//         void read(std::ifstream &img);
-// };
-
 class IHDR : public base_chunk {
     public:
         IHDR(std::ifstream &img, const unsigned int size, const byte_t type[4]);
