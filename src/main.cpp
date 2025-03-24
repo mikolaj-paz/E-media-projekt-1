2bcf4318a23c1f1b5253489f7c589172efadb36f
Mikolaj <mikolajpaz@gmail.com>
2025-03-24 23:00:24 +0100

    Fourier transform magnitude chart

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <fftw3.h>
 
 #ifndef IMAGE_DIR
-#define IMAGE_DIR "../example2.png"
+#define IMAGE_DIR "../example5.png"
 #endif
 
 int main() {
@@ -20,50 +20,55 @@ int main() {
         return 0;
     }
 
-    const sf::Image image("../cln1.gif");
+    const sf::Image image(IMAGE_DIR);
     unsigned int width = image.getSize().x;
     unsigned int height = image.getSize().y;
 
-    sf::RenderWindow window(sf::VideoMode({width, height}), "Obraz");
+    sf::RenderWindow window(sf::VideoMode({2 * width, height}), "Obraz");
 
     fftw_complex* complexData = fftw_alloc_complex(width * height);
     fftw_complex* fft = fftw_alloc_complex(width * height);
 
-    fftw_plan plan = fftw_plan_dft_1d(width * height, complexData, fft, FFTW_FORWARD, FFTW_ESTIMATE);
+    // fftw_plan plan = fftw_plan_dft_1d(width * height, complexData, fft, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_plan plan = fftw_plan_dft_2d(width, height, complexData, fft, FFTW_FORWARD, FFTW_ESTIMATE);
 
+    double maxVal = 0.0;
     for (unsigned int i = 0, k = 0; i < height; i++)
         for (unsigned int j = 0; j < width; j++) {
-            complexData[k][0] = static_cast<double>(image.getPixel({j, i}).r);
+            double r = static_cast<double>(image.getPixel({j, i}).r);
+            double g = static_cast<double>(image.getPixel({j, i}).g);
+            double b = static_cast<double>(image.getPixel({j, i}).b);
+
+            complexData[k][0] = (r + g + b) / 3.0f;
             complexData[k++][1] = 0.0;
         }
 
     fftw_execute(plan);
     fftw_destroy_plan(plan);
 
-    // double max = 0.0, min = 99999999.0, avg = 0.0; 
-    // for (int i = 0; i < width * height; i++) {
-    //     double val = std::log(1.0 + fft[i][0]);
-    //     if (max < val) max = val;
-    //     if (min > val) min = val;
-    //     avg += val;
-    // }
-    // avg /= static_cast<double>(width * height);
-    // std::cout << "max: " << max << " min: " << min << " avg: " << avg << std::endl;
-
+    double magnitudes[width * height];
     sf::Image fftImage({width, height});
     for (unsigned int i = 0; i < height; i++)
         for (unsigned int j = 0; j < width; j++) {
-            double real = fft[i * width + j][0] / static_cast<double>(width * height);
-            double imag = fft[i * width + j][1] / static_cast<double>(width * height);
-            unsigned char grey = static_cast<unsigned char>(std::sqrt(real * real + imag * imag));
-            // std::cout << static_cast<int>(grey) << " ";
-            fftImage.setPixel({j, i}, sf::Color(grey, grey, grey));
+            double real = fft[i * width + j][0];
+            double imag = fft[i * width + j][1];
+            double grey = std::sqrt(real * real + imag * imag);
+            magnitudes[i * width + j] = grey;
+            if (maxVal < grey) maxVal = grey;
         }
 
-    std::cout << std::endl;
-
-    const sf::Texture texture(fftImage);
+    for (unsigned int i = 0; i < height; i++)
+        for (unsigned int j = 0; j < width; j++) {
+            unsigned char grey = static_cast<unsigned char>(255.0 * (std::log(magnitudes[i * width + j]) / std::log(maxVal)));
+            fftImage.setPixel({(j + width / 2) % width, (i + height / 2) % height }, sf::Color(grey, grey, grey));
+        }
+    
+    const sf::Texture texture(image);
+    const sf::Texture fftTexture(fftImage);
     sf::Sprite sprite(texture);
+    sf::Sprite fftSprite(fftTexture);
+
+    fftSprite.move({static_cast<float>(width), 0});
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -73,6 +78,7 @@ int main() {
 
         window.clear();
         window.draw(sprite);
+        window.draw(fftSprite);
         window.display();
     }    
 
